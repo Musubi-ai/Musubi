ad8f1a13e37da014354374efbb77e5668d988be3
Blaze <blaze7451@gmail.com>
2024-11-10 06:54:23 +0800

    add scroll class into crawl_link.py

--- src/crawl_link.py
+++ src/crawl_link.py
@@ -1,9 +1,13 @@
 import os
 import requests
+from selenium.webdriver import Edge
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.by import By
 import pandas as pd
 from bs4 import BeautifulSoup
 from typing import List
 import json
+import time
 from tqdm import tqdm
 
 headers = {'user-agent': 'Mozilla/5.0'}
@@ -87,18 +91,101 @@ class Scan():
         print(link_list[0])
 
 
+class Scroll:
+    def __init__(
+        self,
+        prefix: str = None,
+        prefix2: str = None,
+        prefix3: str = None,
+        pages: int = None,
+        block1: List[str] = None,
+        block2: List[str] = None,
+        url_path: str = None,
+        sleep_time: int = 3,
+    ):
+        self.prefix = prefix
+        self.prefix2 = prefix2
+        self.prefix3 = prefix3
+        self.url_path = url_path
+        self.block1 = block1
+        self.block2 = block2
+        self.sleep_time = sleep_time
+        self.scroll_time = pages
+        self.url_path = url_path
+
+    def browse_website(self):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920x1080")
+        self.driver = Edge(options=options)
+        self.driver.get(self.prefix)
+        time.sleep(self.sleep_time)
+
+    def scroll(
+        self,
+        scroll_time: int = None
+    ):
+        n = 0
+        scroll_time = scroll_time if scroll_time is not None else self.scroll_time
+        pbar = tqdm(total = scroll_time)
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        while n < scroll_time:
+            self.driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+            n += 1
+            time.sleep(self.sleep_time)
+            pbar.update(1)
+
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+    def crawl_link(self):
+        self.browse_website()
+        self.scroll()
+        element = self.driver.find_element(By.CLASS_NAME, self.block1[1])
+        elements = element.find_elements(By.TAG_NAME, "a")
+
+        for item in elements:
+            url = item.get_attribute("href")
+            if self.prefix3:
+                url = self.prefix3 + url
+            dictt = {"link": url}
+
+            with open(self.url_path, "a+", encoding="utf-8") as file:
+                file.write(json.dumps(dictt, ensure_ascii=False) + "\n")
+
+    def check_link_result(self):
+        self.browse_website()
+        self.scroll(scroll_time = 1)
+        element = self.driver.find_element(By.CLASS_NAME, self.block1[1])
+        elements = element.find_elements(By.TAG_NAME, "a")
+
+        check_list = []
+
+        for item in elements:
+            url = item.get_attribute("href")
+            if self.prefix3:
+                url = self.prefix3 + url
+            dictt = {"link": url}
+            check_list.append(dictt)
+        
+        print(check_list)
+
+
 if __name__ == "__main__":
-    prefix =  "https://aroundtaiwan.net/category/go/page/"
-    prefix2 = "/"
+    prefix =  "https://www.popdaily.com.tw/explore/press"
+    prefix2 = None
     prefix3 = None
-    pages = 6
-    block1 = ["div", "entries"]
-    block2 = ["h2", "entry-title"]
+    pages = 5
+    block1 = ["div", "DesktopGridContainer-sc-1eawaw9-0.cutvQW"]
+    block2 = None
     url_path = "test.json"
-    scan = Scan(prefix, prefix2, prefix3, pages, block1, block2, url_path)
+    # scan = Scan(prefix, prefix2, prefix3, pages, block1, block2, url_path)
     # scan.check_link_reslt()
-    scan.crawl_link()
-    
-
-
-
+    # scan.crawl_link()
+    scroll = Scroll(prefix, prefix2, prefix3, pages, block1, block2, url_path)
+    # scroll.check_link_result()
+    scroll.crawl_link()    

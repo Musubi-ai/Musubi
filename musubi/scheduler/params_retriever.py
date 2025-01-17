05941595d4d2b27a3e23591044dc692729717447
Blaze <blaze7451@gmail.com>
2025-01-17 22:03:17 +0800

    finish developement

---
+++ musubi/scheduler/params_retriever.py
@@ -0,0 +1,73 @@
+def get_cron_params():
+        """
+        Retrieve cron arguments from user's input.
+        Reference for cron expression:
+        https://blog.csdn.net/devil6636252/article/details/109113104
+        https://www.quartz-scheduler.org/documentation/quartz-2.3.0/tutorials/crontrigger.html
+        """
+        print("\nEnter cron arguments（Or just press Enter to input default value '*'）:")
+        params = {}
+        cron_fields = ['year', 'month', 'day', 'week', 'day_of_week', 'hour', 'minute', 'second']
+        
+        for field in cron_fields:
+            value = input(f"{field}: ").strip()
+            params[field] = value if value else '*'
+
+        print("\nThe cron arguments are：")
+        for field, value in params.items():
+            print(f"{field}: {value}")
+            
+        confirm = input("\nContinue(y/n): ").lower()
+        if confirm != 'y':
+            print("Cancel adding new task")
+            return None
+
+        return params
+
+
+def get_idx_task_params():
+    print("\nEnter task arguments（the arguments are same as those of start_by_idx function）:")
+    params = {}
+    cron_fields = ["idx", "upgrade_pages", "save_dir", "task_name"]
+
+    for field in cron_fields:
+        value = input(f"{field}: ").strip()
+        if (field == "idx") or (field == "upgrade_pages"):
+            params[field] = int(value) if value else None
+        else:
+            params[field] = value if value else None
+
+    print("\nThe start_by_idx arguments are：")
+    for field, value in params.items():
+        print(f"{field}: {value}")
+        
+    confirm = input("\nStart the new task(y/n): ").lower()
+    if confirm != 'y':
+        print("Cancel adding new task")
+        return None
+
+    return params
+
+
+def get_upgrade_task_params():
+    print("\nEnter task arguments（the arguments are same as those of start_all function）:")
+    params = {}
+    cron_fields = ["start_idx", "upgrade_pages", "save_dir", "task_name"]
+
+    for field in cron_fields:
+        value = input(f"{field}: ").strip()
+        if (field == "idx") or (field == "upgrade_pages"):
+            params[field] = int(value) if value else None
+        else:
+            params[field] = value if value else None
+
+    print("\nThe start_all arguments are：")
+    for field, value in params.items():
+        print(f"{field}: {value}")
+        
+    confirm = input("\nStart the new task(y/n): ").lower()
+    if confirm != 'y':
+        print("Cancel adding new task")
+        return None
+
+    return params
--- musubi/scheduler/scheduler.py
+++ musubi/scheduler/scheduler.py
@@ -1,50 +1,22 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from ..pipeline import Pipeline
 from .notification import Notify
+from .params_retriever import get_cron_params, get_idx_task_params, get_upgrade_task_params
 from typing import Optional
 import os
 import uuid
 import time
 from datetime import datetime
+from abc import ABC, abstractmethod
 
 
-def get_cron_params():
-        """
-        Retrieve cron arguments from user's input.
-        Reference for cron expression:
-        https://blog.csdn.net/devil6636252/article/details/109113104
-        https://www.quartz-scheduler.org/documentation/quartz-2.3.0/tutorials/crontrigger.html
-        """
-        print("\nEnter cron arguments（Press Enter to enter default value '*'）：")
-        params = {}
-        cron_fields = ['year', 'month', 'day', 'week', 'day_of_week', 'hour', 'minute', 'second']
-        
-        for field in cron_fields:
-            value = input(f"{field}: ").strip()
-            params[field] = value if value else '*'
-
-        print("\nThe cron arguments are：")
-        for field, value in params.items():
-            print(f"{field}: {value}")
-            
-        confirm = input("\nStart the new task(y/n): ").lower()
-        if confirm != 'y':
-            print("Cancel adding new task")
-            return None
-
-        return params
-
-
-class CrawlScheduler:
-    """
-    Periodically crawl certain website based on idx.
-    """
+class BaseScheduler(ABC):
     def __init__(
         self,
-        send_notification: bool = False,
+        send_notification: Optional[bool] = False,
         app_password: Optional[str] = None,
         sender_email: Optional[str] = None,
-        recipient_email: Optional[str] = None,
+        recipient_email: Optional[str] = None, 
     ):
         if send_notification:
             if app_password is not None:
@@ -59,15 +31,30 @@ class CrawlScheduler:
                 recipient_email=recipient_email
             )
 
-    def task(
+    @abstractmethod
+    def launch_scheduler(self):
+        ...
+    
+
+class IdxScheduler(BaseScheduler):
+    """
+    Periodically crawl certain website based on idx.
+    """
+    def __init__(
+        self,
+        send_notification: Optional[bool] = False,
+        app_password: Optional[str] = None,
+        sender_email: Optional[str] = None,
+        recipient_email: Optional[str] = None,
+    ):
+        super().__init__(send_notification, app_password, sender_email, recipient_email)
+
+    def start_by_idx_task(
         self,
         website_path="config/websites.json",
-        start_page: Optional[int] = 0,
-        start_idx: Optional[int] = 0,
-        idx: Optional[int] = None,
+        idx: int = None,
         upgrade_pages: Optional[int] = None,
-        sleep_time: Optional[int] = None,
-        save_dir: str = None,
+        save_dir: Optional[str] = None,
         task_name: str = None
     ):
         if self.notify:
@@ -78,11 +65,8 @@ class CrawlScheduler:
 
         pipe = Pipeline(website_path=website_path)
         pipe.start_by_idx(
-            start_page=start_page,
-            start_idx=start_idx,
             idx=idx,
             upgrade_pages=upgrade_pages,
-            sleep_time=sleep_time,
             save_dir=save_dir
         )
 
@@ -92,15 +76,7 @@ class CrawlScheduler:
                 body="Finished scheduled task {} at {}".format(task_name, datetime.now())
             )
     
-    def launch_scheduler(
-        self,
-        start_page: Optional[int] = 0,
-        start_idx: Optional[int] = 0,
-        idx: Optional[int] = None,
-        upgrade_pages: Optional[int] = None,
-        sleep_time: Optional[int] = None,
-        save_dir: str = None
-    ):
+    def launch_scheduler(self):
         scheduler = BackgroundScheduler()
         scheduler.start()
 
@@ -114,22 +90,16 @@ class CrawlScheduler:
                     try:
                         task_name = input("Task name: ").strip()
                         cron_params = get_cron_params()
+                        task_params = get_idx_task_params()
                         # Generate unique task id by uuid4
                         job_id = uuid.uuid4()
                         
                         # Add task
                         scheduler.add_job(
-                            self.task, 
+                            self.start_by_idx_task, 
                             'cron', 
                             id=job_id, 
-                            kwargs={
-                                "start_page": start_page, 
-                                "start_idx": start_idx, 
-                                "idx": idx, 
-                                "upgrade_pages": upgrade_pages, 
-                                "sleep_time": sleep_time,
-                                "save_dir": save_dir
-                            },
+                            kwargs=task_params,
                             **cron_params
                         )
                         active_jobs[job_id] = task_name
@@ -179,3 +149,113 @@ class CrawlScheduler:
             scheduler.shutdown()
             print("Shut down scheduler.")
 
+
+class UpgradeScheduler(BaseScheduler):
+    def __init__(
+        self, 
+        send_notification: Optional[bool] = False,
+        app_password: Optional[str] = None,
+        sender_email: Optional[str] = None,
+        recipient_email: Optional[str] = None, 
+    ):
+        super().__init__(send_notification, app_password, sender_email, recipient_email)
+
+    def start_all_task(
+        self,
+        website_path="config/websites.json",
+        start_idx: Optional[int] = 0,
+        upgrade_pages: Optional[int] = None,
+        save_dir: Optional[str] = None,
+        task_name: str = None
+    ):
+        if self.notify:
+            self.notify.send_gmail(
+                subject="Musubi: Start scheduled crawling",
+                body="Start scheduled task {} at {}".format(task_name, datetime.now())
+            )
+
+        pipe = Pipeline(website_path=website_path)
+        pipe.start_all(
+            start_idx=start_idx,
+            upgrade_pages=upgrade_pages,
+            save_dir=save_dir
+        )
+
+        if self.notify:
+            self.notify.send_gmail(
+                subject="Musubi: Finished scheduled crawling",
+                body="Finished scheduled task {} at {}".format(task_name, datetime.now())
+            )
+
+    def launch_scheduler(self):
+        scheduler = BackgroundScheduler()
+        scheduler.start()
+
+        active_jobs = {}
+
+        try:
+            while True:
+                command = input("\nInput command ('add', 'pause', 'resume', 'list', 'exit'): ").strip().lower()
+                
+                if command == 'add':
+                    try:
+                        task_name = input("Task name: ").strip()
+                        cron_params = get_cron_params()
+                        task_params = get_upgrade_task_params()
+                        # Generate unique task id by uuid4
+                        job_id = uuid.uuid4()
+                        
+                        # Add task
+                        scheduler.add_job(
+                            self.start_all_task, 
+                            'cron', 
+                            id=job_id, 
+                            kwargs=task_params,
+                            **cron_params
+                        )
+                        active_jobs[job_id] = task_name
+                        print(f"Add task '{task_name}', ID: {job_id}")
+
+                    except ValueError as e:
+                        print(f"ValueError: {str(e)}")
+                        continue
+                    except Exception as e:
+                        print(f"Error: {str(e)}")
+                        continue
+                
+                elif command == 'pause':
+                    job_id = input("Task ID: ").strip()
+                    if job_id in active_jobs:
+                        scheduler.pause_job(job_id)
+                        print(f"Pause task '{active_jobs[job_id]}'.")
+                    else:
+                        print("Cannot find the task having ID {}!".format(job_id))
+                
+                elif command == 'resume':
+                    job_id = input("Enter task ID: ").strip()
+                    if job_id in active_jobs:
+                        scheduler.resume_job(job_id)
+                        print(f"Task '{active_jobs[job_id]}' has been resumed.")
+                    else:
+                        print("Cannot find task ID!")
+                
+                elif command == 'list':
+                    print("Current task list：")
+                    for job_id, task_name in active_jobs.items():
+                        job = scheduler.get_job(job_id)
+                        status = "pausing" if job.next_run_time is None else "operating"
+                        print(f"  - ID: {job_id}, Nmae: {task_name}, Status: {status}")
+                
+                elif command == 'exit':
+                    print("Shut downing Scheduler...")
+                    scheduler.shutdown()
+                    break
+                
+                else:
+                    print("Invalid command. The command should be one of 'add', 'pause', 'resume', 'list', 'exit', but got {}".format(command))
+                
+                time.sleep(0.5)
+
+        except (KeyboardInterrupt, SystemExit):
+            scheduler.shutdown()
+            print("Shut down scheduler.")

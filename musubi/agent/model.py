97ba76f0768b9ed5fc8aa3f8d192e26fd802bdf0
Blaze <blaze7451@gmail.com>
2025-02-12 01:16:13 +0800

    TODO: support various models in agent

--- musubi/agent/model.py
+++ musubi/agent/model.py
@@ -1,10 +1,11 @@
 import openai
+import groq
+from abc import ABC, abstractmethod
 import os
 from dotenv import load_dotenv
 
 
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 class OpenAIModel:
@@ -12,17 +13,22 @@ class OpenAIModel:
     Wrapper class for OpenAI's Chat API. Designed by Simon
     '''
     def __init__(
-        self, 
+        self,
+        openai_api: str = None,
         system_prompt: str = None, 
-        model_api: str = None
+        model: str = None
     ):
+        if openai_api is not None:
+            openai_api = openai_api
+        else:
+            openai_api = openai.api_key = os.getenv('OPENAI_API_KEY')
         self.system_prompt = system_prompt
-        self.model_api = model_api
+        self.model = model
         self.messages = []
         if self.system_prompt is not None:
             self.messages.append({"role": "system", "content": self.system_prompt})
-        if self.model_api is None:
-            self.model_api = "chatgpt-4o-latest"
+        if self.model is None:
+            self.model = "chatgpt-4o-latest"
 
     def __call__(self, message):
         self.messages.append({"role": "user", "content": message})
@@ -32,7 +38,7 @@ class OpenAIModel:
     
     def exceute(self):
         completion = openai.chat.completions.create(
-        model=self.model_api,
+        model=self.model,
         messages=self.messages
         )
         response = completion.choices[0].message.content

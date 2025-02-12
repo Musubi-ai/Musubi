df35dbd8d7b24c20c1c5eb83dbb8908bd1ef9157
aaron <blaze7451@gmail.com>
2025-02-12 16:40:23 +0800

    add groq and deepseek model

--- musubi/agent/model.py
+++ musubi/agent/model.py
@@ -61,3 +61,81 @@ class OpenAIModel(BaseModel):
         **generate_kwargs
         )
         return completion.choices[0].message.content
+
+
+class GroqModel(BaseModel):
+    def __init__(
+        self, 
+        api_key: str,
+        system_prompt: Optional[str] = None, 
+        model: Optional[str] = None
+    ):
+        super().__init__(system_prompt, model)
+        self.api_key = api_key or os.getenv("GROQ_API_KEY")
+        if not self.api_key:
+            raise ValueError("API Key is required for GroqModel.")
+        if self.model is None:
+            self.model = "llama-3.3-70b-versatile"
+        self.client = groq.Groq(api_key=self.api_key)
+    
+    def execute(self, **generate_kwargs):
+        completion = self.client.chat.completions.create(
+        model=self.model,
+        messages=self.messages,
+        **generate_kwargs
+        )
+        return completion.choices[0].message.content
+    
+
+class GrokModel(BaseModel):
+    def __init__(
+        self, 
+        api_key: str,
+        system_prompt: Optional[str] = None, 
+        model: Optional[str] = None
+    ):
+        super().__init__(system_prompt, model)
+        self.api_key = api_key or os.getenv("XAI_API_KEY")
+        if not self.api_key:
+            raise ValueError("API Key is required for GrokModel.")
+        if self.model is None:
+            self.model = "grok-2-latest"
+        self.client = openai.OpenAI(
+            api_key=self.api_key,
+            base_url="https://api.x.ai/v1",
+        )
+    
+    def execute(self, **generate_kwargs):
+        completion = self.client.chat.completions.create(
+        model=self.model,
+        messages=self.messages,
+        **generate_kwargs
+        )
+        return completion.choices[0].message.content
+    
+
+class DeepseekModel(BaseModel):
+    def __init__(
+        self, 
+        api_key: str,
+        system_prompt: Optional[str] = None, 
+        model: Optional[str] = None
+    ):
+        super().__init__(system_prompt, model)
+        self.api_key = api_key or os.getenv("DEEPSEEK_API_Key")
+        if not self.api_key:
+            raise ValueError("API Key is required for DeepseekModel.")
+        if self.model is None:
+            self.model = "deepseek-chat"
+        self.client = openai.OpenAI(
+            api_key=self.api_key,
+            base_url="https://api.deepseek.com",
+        )
+    
+    def execute(self, **generate_kwargs):
+        completion = self.client.chat.completions.create(
+        model=self.model,
+        messages=self.messages,
+        **generate_kwargs
+        )
+        return completion.choices[0].message.content

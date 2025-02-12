ff595258592e918a1bff27f34b6fa8b9f8b019fe
aaron <blaze7451@gmail.com>
2025-02-12 15:28:58 +0800

    edit model.py

--- musubi/agent/model.py
+++ musubi/agent/model.py
@@ -1,5 +1,6 @@
 import openai
 import groq
+from typing import Optional
 from abc import ABC, abstractmethod
 import os
 from dotenv import load_dotenv
@@ -8,38 +9,60 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class OpenAIModel:
-    '''
-    Wrapper class for OpenAI's Chat API. Designed by Simon
-    '''
+class BaseModel(ABC):
+    """Wrapper class for Model's API."""
     def __init__(
         self,
-        openai_api: str = None,
-        system_prompt: str = None, 
-        model: str = None
+        system_prompt: Optional[str] = None, 
+        model: Optional[str] = None
     ):
-        if openai_api is not None:
-            openai_api = openai_api
-        else:
-            openai_api = openai.api_key = os.getenv('OPENAI_API_KEY')
         self.system_prompt = system_prompt
         self.model = model
         self.messages = []
         if self.system_prompt is not None:
             self.messages.append({"role": "system", "content": self.system_prompt})
-        if self.model is None:
-            self.model = "chatgpt-4o-latest"
 
     def __call__(self, message):
         self.messages.append({"role": "user", "content": message})
-        result = self.exceute()
+        result = self.execute()
         self.messages.append({"role": "assistant", "content": result})
         return result
     
-    def exceute(self):
-        completion = openai.chat.completions.create(
+    @abstractmethod
+    def execute(self):
+        """Abstract method for executing API calls."""
+        pass
+
+
+class OpenAIModel(BaseModel):
+    def __init__(
+        self, 
+        api_key: str,
+        system_prompt: Optional[str] = None, 
+        model: Optional[str] = None
+    ):
+        super().__init__(system_prompt, model)
+        api_key = api_key if api_key else os.getenv("OPENAI_API_KEY")
+        if self.model is None:
+            self.model = "chatgpt-4o-latest"
+        self.client = openai.OpenAI(api_key=api_key)
+
+    def __call__(
+        self, 
+        message: str
+    ):
+        if not isinstance(message, str):
+            raise ValueError("The message should be string.")
+        self.messages.append({"role": "user", "content": message})
+        result = self.execute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+    
+    def execute(self, **generate_kwargs):
+        completion = self.client.chat.completions.create(
         model=self.model,
-        messages=self.messages
+        messages=self.messages,
+        **generate_kwargs
         )
         response = completion.choices[0].message.content
         return response

051aaadd75f340b6766a16e14d5292df1e28f59c
Blaze <blaze7451@gmail.com>
2025-02-13 23:27:17 +0800

    editing system prompt

--- musubi/agent/models.py
+++ musubi/agent/models.py
@@ -1,6 +1,7 @@
 import openai
 import groq
 import anthropic
+from huggingface_hub import InferenceClient
 from typing import Optional
 from abc import ABC, abstractmethod
 import os
@@ -193,6 +194,7 @@ class ClaudeModel(BaseModel):
         )
         return completion.content[0].text
 
+
 class GeminiModel(BaseModel):
     def __init__(
         self, 
@@ -222,13 +224,44 @@ class GeminiModel(BaseModel):
         return completion.choices[0].message.content
 
 
-#TODO: Add huggingface inference api model
-# Check https://huggingface.co/docs/huggingface_hub/package_reference/inference_client
+class HFModel(BaseModel):
+    def __init__(
+        self, 
+        api_key: Optional[str] = None,
+        system_prompt: Optional[str] = None, 
+        model: Optional[str] = None
+    ):
+        super().__init__()
+        self.api_key = api_key if api_key else os.getenv("HF_TOKEN")
+        if not self.api_key:
+            raise ValueError("API Key is required for HFModel.")
+        self.messages = []
+        self.system_prompt = system_prompt
+        if self.system_prompt is not None:
+            self.messages.append({"role": "system", "content": self.system_prompt})
+        self.model = model
+        if self.model is None:
+            self.model = "Qwen/Qwen2.5-Coder-32B-Instruct"
+        self.client = InferenceClient(
+            model=self.model,
+            api_key=self.api_key,
+        )
+    
+    def execute(self, **generate_kwargs):
+        completion = self.client.chat.completions.create(
+        model=self.model,
+        messages=self.messages,
+        **generate_kwargs
+        )
+        return completion.choices[0].message.content
+
+
 MODEL_NAMES={
     "openai": OpenAIModel,
     "groq": GroqModel,
     "xai": GrokModel,
     "deepseek": DeepseekModel,
     "anthropic": ClaudeModel,
-    "google": GeminiModel
+    "google": GeminiModel,
+    "huggingface": HFModel
 }
--- musubi/agent/system_prompt.py
+++ musubi/agent/system_prompt.py
@@ -1,105 +1,88 @@
-TOOL_CALLING_SYSTEM_PROMPT = """You are a crawling expert assistant who can implement any crawling task using action calls. You will be given a task to solve as best you can.
+TOOL_CALLING_SYSTEM_PROMPT = """You are a crawling expert assistant who can implement any crawling task using special crawling `pipeline` function.
+Here is the description of the pipeline function:
+{{pipeline_description}}
+
+Before executing pipeline function, you have to take various action calls to retrieve the argumets of pipeline function.
 To do so, you have been given access to the following actions: {{action_names}}. 
-Note that before taking action, you should implement reasoning and write down your thought about the question you have been asked and how to solve it.
+Note that before taking actions, you should implement reasoning and output your thought about the question you have been asked and how to solve it.
 
 The action call you write is an action step: after the action is executed, you will get the result of the action call as an "observation".
 This Thought-Action-Observation chain can repeat N times, you should take several steps when needed.
 
 You can use the result of the previous action as input for the next action.
-The observation will always be a string: it can represent a URL, like "image_1.jpg".
+The observation will always be a string: it can represent a URL, like "https://lithub.com/".
 Then you can use it as input for the next action. You can do it for instance as follows:
 
-Observation: "image_1.jpg"
+Observation: "https://lithub.com/"
 
-Step:
+Action:
 {
-  "action_name": "image_transformer",
-  "tool_arguments": {"image": "image_1.jpg"}
+  "action_name": "analyze_website",
+  "action_arguments": {"url": "https://lithub.com/"}
 }
 
 To provide the final answer to the task, use an action blob with "action_name": "final_answer" tool. It is the only way to complete the task, else you will be stuck on a loop. So your final output should look like this:
 Action:
 {
   "action_name": "final_answer",
-  "tool_arguments": {"answer": "insert your final answer here"}
+  "action_arguments": {"answer": {"dir": "test", "name": "test", "class_": "中文", "prefix": "...", "suffix": None, "root_path": None, ...}}
 }
-
 
 Here is the typical example using action tools:
 ---
-Task: "Generate an image of the oldest person in this document."
+Task: "Scrape articles from the 'Fiction and Poetry' category on Literary Hub, from page 1 to page 5."
 
-Action:
+Thought: Alright, 
+
 {
-  "action_name": "document_qa",
-  "tool_arguments": {"document": "document.pdf", "question": "Who is the oldest person mentioned?"}
+  "behaviour": "action",
+  "action_name": "google_search",
+  "action_arguments": {"query": "Literary Hub Fiction and Poetry"}
 }
-Observation: "The oldest person in the document is John Doe, a 55 year old lumberjack living in Newfoundland."
+
+Observation: "('https://lithub.com/category/fictionandpoetry/', 'https://lithub.com')"
 
 Action:
 {
   "action_name": "image_generator",
-  "tool_arguments": {"prompt": "A portrait of John Doe, a 55-year-old man living in Canada."}
+  "action_arguments": {"prompt": "A portrait of John Doe, a 55-year-old man living in Canada."}
 }
 Observation: "image.png"
 
 Action:
 {
   "action_name": "final_answer",
-  "tool_arguments": "image.png"
+  "action_arguments": "image.png"
 }
 
 ---
-Task: "What is the result of the following operation: 5 + 3 + 1294.678?"
+Task: "爬取"
 
 Action:
 {
     "action_name": "python_interpreter",
-    "tool_arguments": {"code": "5 + 3 + 1294.678"}
+    "action_arguments": {"code": "5 + 3 + 1294.678"}
 }
 Observation: 1302.678
 
 Action:
 {
   "action_name": "final_answer",
-  "tool_arguments": "1302.678"
+  "action_arguments": "1302.678"
 }
 
 ---
-Task: "Which city has the highest population , Guangzhou or Shanghai?"
-
-Action:
-{
-    "action_name": "search",
-    "tool_arguments": "Population Guangzhou"
-}
-Observation: ['Guangzhou has a population of 15 million inhabitants as of 2021.']
-
-
-Action:
-{
-    "action_name": "search",
-    "tool_arguments": "Population Shanghai"
-}
-Observation: '26 million (2019)'
-
-Action:
-{
-  "action_name": "final_answer",
-  "tool_arguments": "Shanghai"
-}
-
 
 Your available actions are:
 
-{{tool_descriptions}}
+{{action_descriptions}}
 
-Here are the rules you should always follow to solve your task:
+Here are the rules you should always follow to finish your task:
 1. ALWAYS provide a action call when Action, else you will fail.
 2. Always use the right arguments for the actions. Never use variable names as the action arguments, use the value instead.
 3. Execute an action call only when needed: do not execute the google_search action if you do not need information, try to solve the task yourself.
 If no action call is needed, use final_answer action to return your answer.
 4. Never re-do a action call that you previously did with the exact same parameters.
 
-Now Begin! If you solve the task correctly, you will receive a reward of $1,000,000.
+Now Begin! If you complete the task correctly, you will receive a reward of $1,000,000.
 """

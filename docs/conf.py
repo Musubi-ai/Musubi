08ea28d677eecdf269c18d6d16f1057f3981b483
Aaron <blaze7451@gmail.com>
2025-10-10 17:45:28 +0800

    doc: change rst to md

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,16 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'musubi-scrape'
-copyright = '2025, Musubi team'
-author = 'Musubi team'
-release = '1.0.2'
+copyright = '2025, Lung Chuan Chen'
+author = 'Lung Chuan Chen'
+release = '1.1.0'
+
+master_doc = "index"
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,40 +27,45 @@ release = '1.0.2'
 extensions = [
     'sphinx.ext.autodoc',          
     'sphinx.ext.napoleon',         
-    'sphinx.ext.viewcode',  
+    'sphinx.ext.viewcode',
+    "sphinx.ext.autosummary",
+    'myst_parser',
+    "sphinx_design"
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+myst_enable_extensions = [
+    "deflist",
+    "html_admonition",
+    "html_image",
+    "colon_fence",
+    "dollarmath",
+    "amsmath",
+    # "linkify",
+    "replacements",
+    "substitution",
+    "tasklist"
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 
-html_logo = '../assets/logo/favicon.png'
+html_logo = '../assets/logo/FullLogo.png'
 html_favicon = '../assets/logo/favicon.png'
 
-# Furo 主題自訂顏色
 html_theme_options = {
-    "sidebar_hide_name": False,
-    "light_css_variables": {
-        "color-brand-primary": "#2E86AB",
-        "color-brand-content": "#2E86AB",
-    },
-    "dark_css_variables": {
-        "color-brand-primary": "#2E86AB",
-        "color-brand-content": "#2E86AB",
-    },
+    "repository_url": "https://github.com/Musubi-ai/Musubi",
+    "use_repository_button": True,
+    "use_fullscreen_button": True,
+    "use_source_button": True,
+    "home_page_in_toc": True
 }
 
-# 側邊欄顯示全部章節
-html_sidebars = {
-   "**": ["sidebar/scroll-start.html",
-          "sidebar/brand.html",
-          "sidebar/navigation.html",
-          "sidebar/scroll-end.html"],
-}
+html_css_files = [
+    "https://unpkg.com/sphinx-design@0.6.0/dist/sphinx-design.min.css",
+]
---
+++ docs/index.md
@@ -0,0 +1,52 @@
+# Musubi-scrape's Documentation
+
+Musubi-scrape is a Python library designed for efficiently crawling and extracting website text, enabling users to construct scalable, domain-specific datasets from the ground up for training LLMs.
+
+With Musubi-scrape, you can:
+
+🕸️ Extract text data from most websites with common structures and transform them into markdown format.
+
+🤖 Deploy AI agents to help you find optimal parameters for website crawling and implement crawling automatically.
+
+📆 Set schedulers to schedule your crawling tasks.
+
+🗂️ Manage crawling configurations for each website conveniently.
+
+We've also developed a CLI tool that lets you crawl and deploy agents without the need to write any code!
+
+---
+
+## Getting Started
+
+...
+
+## Guides
+...
+
+## Python API
+...
+
+---
+
+## Aditional resources
+
+```{admonition} This is my admonition
+This is my note
+```
+
+
+:::{card} Card Title
+Header
+^^^
+Card content
++++
+Footer
+:::
+
+
+```{toctree}
+:maxdepth: 2
+:caption: Contents
+
+guide/guide
+```
